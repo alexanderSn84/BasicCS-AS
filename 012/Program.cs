724fc3b7c8833945d9108cf0fcd4c64ac40ace95
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 22:29:52 +0300

    Задача № 012

---
+++ 012/Program.cs
@@ -0,0 +1,11 @@
+﻿// 012. Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int a = new Random().Next(10, 99);
+Console.WriteLine(a);
+
+int d1 = a / 10;
+int d2 = a % 10;
+
+int m = Math.Max(d1, d2);
+Console.Write("max = ");
+Console.WriteLine(m);
