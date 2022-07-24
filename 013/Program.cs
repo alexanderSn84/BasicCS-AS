e210bb96cdbeb34bc1f1c7d5f8fafd1531ac569d
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 22:44:50 +0300

    Задача № 013

---
+++ 013/Program.cs
@@ -0,0 +1,8 @@
+﻿// 013. Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+int b=a/100; // Первая цифра
+int c=a%10; // Последняя цифра
+Console.WriteLine($"{b}{c} ");
