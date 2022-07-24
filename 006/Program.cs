a168ec7a3583bc7d03862ef451f0291d51cef002
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 21:48:22 +0300

    Задача № 006

---
+++ 006/Program.cs
@@ -0,0 +1,23 @@
+﻿// 006. Написать программу вычисления значения функции y = f(a)
+
+Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int y;
+
+if(a>0)
+{
+    y=a*a+3;
+}
+else
+{
+    if(a==0)
+    {
+        y=0;
+    }
+    else
+    {
+        y=Math.Abs(a);
+    }
+}
+
+Console.WriteLine($"y = {y}");
