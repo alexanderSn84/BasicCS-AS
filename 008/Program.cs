6bcb8b1c0cf89f65f7228c069e5fe4b9fb76e5df
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 22:12:40 +0300

    Задача № 008

---
+++ 008/Program.cs
@@ -0,0 +1,27 @@
+﻿// 008. Показать числа от -N до N
+int a;
+int b;
+int razn;
+int i=0;
+Console.Write("Введите первое число: ");
+a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b=Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    razn=a-b;
+    while(i<=razn)
+    {
+        Console.Write($"{b+i} ");
+        i=i+1;
+    }
+}
+else
+{
+    razn=b-a;
+    while(i<=razn)
+    {
+        Console.Write($"{a+i} ");
+        i=i+1;
+    }
+}
