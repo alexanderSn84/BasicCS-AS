7cc30476cd8b98eb22a28cb05f42c86416279b98
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 21:45:28 +0300

    Задача № 005

---
+++ 005/Program.cs
@@ -0,0 +1,24 @@
+﻿// 005. Найти максимальное из трех чисел
+
+int a;
+int b;
+int c;
+Console.Write("Введите первое число: ");
+a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+c=Convert.ToInt32(Console.ReadLine());
+
+if((a>=b) && (a>=c))
+{
+    Console.WriteLine($"max = {a}");
+}
+else if((a<=b) && (b>=c))
+{
+    Console.WriteLine($"max = {b}");
+}
+else
+{
+    Console.WriteLine($"max = {c}");
+}
