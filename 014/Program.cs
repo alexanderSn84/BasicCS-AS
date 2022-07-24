0e57613c6cde7a4f0c5594150b7950bf8d1f688b
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 22:47:07 +0300

    Задача № 014

---
+++ 014/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите делитель числа: ");
+int b=Convert.ToInt32(Console.ReadLine());
+
+if(a%b==0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    Console.WriteLine(a%b);
+}
