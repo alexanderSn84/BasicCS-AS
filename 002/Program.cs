b83dc459836aa482b50f1bc14838ec5b9f81dadb
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 21:35:31 +0300

    Задача № 002

---
+++ 002/Program.cs
@@ -0,0 +1,25 @@
+﻿// 002. По двум заданным числам проверять является ли первое квадратом второго
+
+double a,b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if(a*a==b)
+{
+    Console.WriteLine($"b={b} is sqare a={a}");
+}
+else
+{
+    Console.WriteLine($"b={b} is't sqare a={a}");
+}
+
+if(a==b*b)
+{
+    Console.WriteLine($"a={a} is sqare b={b}");
+}
+else
+{
+    Console.WriteLine($"a={a} is't sqare b={b}");
+}
