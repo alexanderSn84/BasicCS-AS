bfe1b8536691af9c7e2e37b51b144b92df4d45e6
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 22:20:20 +0300

    Задача № 011

---
+++ 011/Program.cs
@@ -0,0 +1,7 @@
+﻿// 011. Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+int b=(a%100)/10;
+Console.WriteLine($"Вторая цифра числа: {b}");
