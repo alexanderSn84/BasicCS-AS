46c181a2eda8da12ccecccb4603dd9ad617ba90a
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 22:17:25 +0300

    Задача № 010

---
+++ 010/Program.cs
@@ -0,0 +1,6 @@
+﻿// 010. Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Последняя цифра: {a%10}"); //Деление с остатком
