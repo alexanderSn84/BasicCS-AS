590ec5a132425656e5c240f37673ab9ae04353e0
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 21:39:02 +0300

    Задача № 003

---
+++ 003/Program.cs
@@ -0,0 +1,16 @@
+﻿// 003. Даны два числа. Показать большее и меньшее число
+
+int a;
+int b;
+Console.Write("Введите первое число: ");
+a=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b=Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine($"{a} > {b}");
+}
+else
+{
+    Console.WriteLine($"{a} < {b}");
+}
