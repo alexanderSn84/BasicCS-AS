f381ea29e3377901ce9f8b527793644a08e97b3b
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-24 21:50:25 +0300

    Задача № 007

---
+++ 007/Program.cs
@@ -0,0 +1,13 @@
+﻿// 007. Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+if(a%2==0) //Деление с остатком
+{
+    Console.WriteLine("Число является чётным");
+}
+else
+{
+    Console.WriteLine("Число нечётное");
+}
