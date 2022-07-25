260785c1daf445a27db22003106547970d9113ad
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:06:02 +0300

    Задача № 023

---
+++ 023/Program.cs
@@ -0,0 +1,12 @@
+﻿// 023.	Найти расстояние между точками в пространстве 2D
+
+double Distance(double x1, double y1, double x2, double y2)
+{
+    double d;
+    d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Sqrt(Math.Pow(y1 - y2, 2)));
+    return d;
+}
+
+double x1 = 6, y1 = 2, x2 = 1, y2 = 1;
+
+Console.WriteLine(Distance(x1, y1, x2, y2));
