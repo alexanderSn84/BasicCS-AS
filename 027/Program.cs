965b275427c01510662cb42c3a17e7dbbd14174e
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:26:44 +0300

    Задача № 027

---
+++ 027/Program.cs
@@ -0,0 +1,17 @@
+﻿// 027.	Возведите число А в натуральную степень B используя цикл
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+int st = 1;
+
+while (i <= b)
+{
+    st *= a;
+    i++;
+}
+
+Console.WriteLine($"{a} ^ {b} = {st}");
