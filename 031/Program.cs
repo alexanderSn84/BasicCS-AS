70ad3b623227e63c04f52a4c6de78ada215f57d4
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:36:16 +0300

    Задача № 031

---
+++ 031/Program.cs
@@ -0,0 +1,18 @@
+﻿// 031.	Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Введите число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число" + "\t" + "Куб");
+
+int i = 1;
+
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(i + "\t" + Math.Pow(Convert.ToDouble(i), 3));
+    }
+    i++;
+}
