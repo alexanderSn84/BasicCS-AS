8c362d2d45579272c0b20084d9b37b64625d4feb
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:32:51 +0300

    Задача № 030

---
+++ 030/Program.cs
@@ -0,0 +1,18 @@
+﻿// 030.	Написать программу вычисления произведения чисел от 1 до N
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int fact(int num)
+{
+    int i = 1;
+    int b = 1;
+    while (i <= num)
+    {
+        b *= i;
+        i++;
+    }
+    return b;
+}
+
+Console.WriteLine($"{a}! = {fact(a)}");
