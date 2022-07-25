2c5ae41a16a4974ffd845f841de655722ae3485d
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:28:39 +0300

    Задача № 028

---
+++ 028/Program.cs
@@ -0,0 +1,15 @@
+﻿// 28.	Определить количество цифр в числе. Сделать подпрограмму.
+
+int CountDigits(int N)
+{
+    if (N == 0) return 1;
+    int k = 0;
+    while (N != 0)
+    {
+        k++;
+        N /= 10;
+    }
+    return k;
+}
+
+Console.WriteLine(CountDigits(1569988));
