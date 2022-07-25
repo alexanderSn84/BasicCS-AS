d55e90b0760b2a6734ad824f1e363fd119be191b
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:20:14 +0300

    Задача № 026

---
+++ 026/Program.cs
@@ -0,0 +1,8 @@
+﻿// 026.	Найти сумму чисел от 1 до А
+
+Console.Write("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int s = (a * (a + 1)) / 2;
+
+Console.WriteLine("Сумма чисел от 1 до " + a + " равна " + s);
