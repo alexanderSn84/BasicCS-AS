d212b00a4791d6728d2a54c2a231f34589499d81
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:08:23 +0300

    Задача № 024

---
+++ 024/Program.cs
@@ -0,0 +1,12 @@
+﻿// 024.	Вывести на экран таблицу квадратов чисел от 1 до N
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= a)
+{
+    Console.WriteLine($"{i} ^ 2 = {Math.Pow(Convert.ToDouble(i), 2)}");
+    i++;
+}
