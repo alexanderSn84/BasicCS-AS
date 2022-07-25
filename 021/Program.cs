b9b36757e0a18789c1c0e5ccf43b54d4de5cd661
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 11:59:27 +0300

    Задача № 021

---
+++ 021/Program.cs
@@ -0,0 +1,23 @@
+﻿// 021.	Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Write("Введите число от 1 до 4: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+        Console.WriteLine("x > 0; y > 0");
+        break;
+    case 2:
+        Console.WriteLine("x < 0; y > 0");
+        break;
+    case 3:
+        Console.WriteLine("x < 0; y < 0");
+        break;
+    case 4:
+        Console.WriteLine("x > 0; y < 0");
+        break;
+    default:
+        Console.WriteLine("Число должно быть от 1 до 4");
+        break;
+}
