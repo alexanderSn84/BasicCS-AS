968592c94fc7ee4f3643d8b207f0efcd5a0d2700
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 11:44:08 +0300

    Задача № 016

---
+++ 016/Program.cs
@@ -0,0 +1,13 @@
+﻿// 016.	Дано число. Проверить кратно ли оно 7 и 23
+
+int a=Convert.ToInt32(Console.ReadLine()); // 7*23=161
+bool b=a%7==0 && a%23==0;
+
+if(b)
+{
+    Console.WriteLine("Кратно");
+}
+else
+{
+    Console.WriteLine("Не кратно");
+}
