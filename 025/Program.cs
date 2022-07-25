5ef6aa9903bf796a9c12d3e11f4af15932b5b9d5
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:17:01 +0300

    Задача № 025

---
+++ 025/Program.cs
@@ -0,0 +1,14 @@
+﻿// 25.	Вывести на экран кубы чисел от 1 до N
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Число" + "\t" + "Куб");
+
+int i = 1;
+
+while (i <= a)
+{
+    Console.WriteLine(i + "\t" + Math.Pow(Convert.ToDouble(i), 3));
+    i++;
+}
