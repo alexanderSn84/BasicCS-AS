c7a127a530f93933d821ca18d85bbecf68deaf6b
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-25 12:30:48 +0300

    Задача № 029

---
+++ 029/Program.cs
@@ -0,0 +1,17 @@
+﻿// 029.	Подсчитать сумму цифр в числе
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int sum(int s)
+{
+    int sum_ch = 0;
+    while (s >= 1)
+    {
+        sum_ch += s % 10;
+        s /= 10;
+    }
+    return sum_ch;
+}
+
+Console.WriteLine($"Сумма цифр в числе равна {sum(a)}");
