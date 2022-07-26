ff5ba3f474236014879acfcc234062a07d7cd4e6
Alexander_Sn84 <alfasnig84@gmail.com>
2022-07-26 14:42:51 +0300

    Задача № 038

---
+++ 038/Program.cs
@@ -0,0 +1,22 @@
+﻿// 38.	В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+int[] a = new int[123];
+Random random = new Random(); //псевдослучайные числа;
+int c = 0;
+
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = random.Next(-200, 300);
+    Console.Write($"{a[i],5}");
+}
+
+Console.WriteLine();
+
+for (int j = 0; j < a.Length; j++)
+{
+    if (a[j] >= 10 && a[j] <= 99)
+    {
+        c += 1;
+    }
+}
+Console.Write($"количество элементов из отрезка [10,99] --> {c}");
