98e4210039357f900eb4318aa9584bf8df36e3b2
Alexander_Sn84 <alfasnig84@gmail.com>
2022-08-11 21:45:45 +0300

    Задача № 046

---
+++ 046/Program.cs
@@ -0,0 +1,17 @@
+﻿// 46.	Показать числа Фибоначчи
+
+Console.Write("Введите длину последовательности: ");
+int dp = Convert.ToInt32(Console.ReadLine());
+long rezultat;
+long f1 = 0;
+long f2 = 1;
+
+for (int i = 0; i <= dp; i++)
+{
+    rezultat = f1;
+    if (i != dp) Console.Write($"{rezultat}, ");
+    else Console.Write(rezultat);
+    f1 += f2;
+    f2 = rezultat;
+}
+Console.WriteLine();
