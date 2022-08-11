a48e56ab1bce850230e3e01be69d18219577c76e
Alexander_Sn84 <alfasnig84@gmail.com>
2022-08-11 21:47:35 +0300

    Задача № 048

---
+++ 048/Program.cs
@@ -0,0 +1,18 @@
+﻿// 48.	Написать программу копирования массива
+
+int[] a = new int[10];
+Random rd = new Random();
+Console.Write("Оригинал \n");
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = rd.Next(0, 100);
+    Console.Write($"{a[i],4}");
+}
+Console.WriteLine();
+Console.Write("Копия \n");
+for (int i = 0; i < a.Length; i++)
+{
+    int copyM = a[i];
+    Console.Write($"{copyM,4}");
+}
+Console.WriteLine();
