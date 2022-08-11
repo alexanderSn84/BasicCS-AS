d70cb1baecf0ef5c91a2ab4418e5d5373d746e89
Alexander_Sn84 <alfasnig84@gmail.com>
2022-08-11 20:26:49 +0300

    with arguments

---
+++ 032_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// 32.	Задать массив из 8 целых элементов и вывести их на экран
+
+//Инициализация переменных
+int[] a;
+
+
+//Ввод данных
+Init(out a, 8, -10, 10);
+//Обработка данных
+
+//Вывод результатов
+Print(a);
+
+
+void Init(out int[] a, int Length, int min, int max)
+{
+    a = new int[Length];
+    Random random = new Random(); //псевдослучайные числа;
+    for (int i = 0; i < a.Length; i++)
+        a[i] = random.Next(min, max + 1);
+}
+
+void Print(int[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+        Console.Write($"{a[i],4}");
+}
