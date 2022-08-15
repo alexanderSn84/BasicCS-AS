386e63ea9741e2518bce5e5b35a10180ec5bc4f6
Alexander_Sn84 <alfasnig84@gmail.com>
2022-08-15 10:31:35 +0300

    Задача № 072

---
+++ 072/Program.cs
@@ -0,0 +1,10 @@
+﻿// 72.	Написать программу возведения числа А в целую степень B
+
+int A = Convert.ToInt32(System.Console.ReadLine());
+int B = Convert.ToInt32(System.Console.ReadLine());
+int Pow1(int A, int B)
+{
+    if (B == 0) return 1;
+    else return Pow1(A, B - 1) * A;
+}
+System.Console.WriteLine(Pow1(A, B));
