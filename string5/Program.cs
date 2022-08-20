b071afd17d4eeb0df39d3827a8e28648154eb281
Alexander_Sn84 <alfasnig84@gmail.com>
2022-08-20 22:24:43 +0300

    Задача string5

---
+++ string5/Program.cs
@@ -0,0 +1,16 @@
+﻿// string5. Определить являются ли введенные с клавиатуры символы 
+// правильно записью целого числа. Вычислить сумму цифр введенного числа.
+
+
+string? s = Console.ReadLine();
+int sum = 0;
+for (int i = 0; i < s!.Length; i++)
+{
+    if (char.IsDigit(s[i])) sum += (int)(char.GetNumericValue(s[i]));
+    else
+    {
+        Console.WriteLine("Не является верной записью целого числа");
+        return;
+    }
+}
+Console.WriteLine($"Сумма цифр числа \t {sum}");
