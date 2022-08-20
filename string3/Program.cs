d193868e4b2103dc36e88c571734886e06d0e2d8
Alexander_Sn84 <alfasnig84@gmail.com>
2022-08-20 22:21:17 +0300

    Задача string3

---
+++ string3/Program.cs
@@ -0,0 +1,14 @@
+﻿// string3. С клавиатуры вводится строка разделенная точкой. 
+// Подсчитать количество символов до точки.
+
+string? text = Console.ReadLine();
+int beforePoints = 0;
+for (int i = 0; text![i] != '.'; i++) beforePoints += 1;
+Console.WriteLine(beforePoints);
+
+
+
+
+// string? text = Console.ReadLine();
+// int beforePoints = text!.IndexOf('.');
+// Console.WriteLine(beforePoints);
